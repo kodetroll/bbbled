522372c57cdb3cdd22ff64422ad461203162f51d
Kodetroll <slm@kodetroll.com>
2015-01-28 18:25:40 -0600

    update colors.sh

--- colors.sh
+++ colors.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# Common Anode LEDs so ON is LOW, OFF is HIGH
 ON=0
 OFF=1
+
+# Map the LED Colors to GPIO pins
 RED=22
 BLUE=23
 GREEN=50
 
 function LED () {
   # LED $1 STATE $2
-  echo "LED (GPIO: $1)"
+  echo "LED (GPIO: $1) ($2)"
   ./gpio_write ${1} ${2}
 }
 
@@ -16,14 +19,14 @@ function RED () {
   LED ${RED} ${1}
 }
 
-function BLU () {
-  # LED $1 STATE $2
-  LED ${BLUE} ${1}
-}
-
 function GRN () {
   # LED $1 STATE $2
   LED ${GREEN} ${1}
+}
+
+function BLU () {
+  # LED $1 STATE $2
+  LED ${BLUE} ${1}
 }
 
 # RED
@@ -32,15 +35,49 @@ RED ${ON}
 sleep 2
 RED ${OFF}
 
+# GREEN
+echo "GREEN"
+GRN ${ON}
+sleep 2
+GRN ${OFF}
+
 # BLUE
 echo "BLUE"
 BLU ${ON}
 sleep 2
 BLU ${OFF}
 
-# GREEN
-echo "GREEN"
+# MAGENTA (RED + BLU)
+echo "MAGENTA"
+BLU ${ON}
+RED ${ON}
+sleep 2
+RED ${OFF}
+BLU ${OFF}
+
+# YELLOW (GRN + BLU)
+echo "YELLOW"
+BLU ${ON}
 GRN ${ON}
 sleep 2
 GRN ${OFF}
+BLU ${OFF}
+
+# CYAN (RED + GRN)
+echo "CYAN"
+RED ${ON}
+GRN ${ON}
+sleep 2
+GRN ${OFF}
+RED ${OFF}
+
+# WHITE (RED + GRN + BLU)
+echo "WHITE"
+RED ${ON}
+GRN ${ON}
+BLU ${ON}
+sleep 2
+GRN ${OFF}
+RED ${OFF}
+BLU ${OFF}
 
