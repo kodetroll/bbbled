e08e786f73142860e386f325fcf9507ed75d6117
kodetroll <slm@kodetroll.com>
2015-01-29 20:17:30 -0600

    fixes, more

--- fade.sh
+++ fade.sh
@@ -25,6 +25,9 @@ check_pinarg $PIN
 UP="10 20 30 40 50 60 70 80 90 100"
 DN="90 80 70 60 50 40 30 20 10 1"
 
+echo "Initializing PWM on ${PIN}"
+./pwm_init.sh ${PIN}
+
 echo "Press CTRL-C to stop"
 while [ 1 ]; do
     for DC in $UP; do
